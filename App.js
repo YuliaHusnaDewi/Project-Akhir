c4733560fb98021a1de0fc36295e2109281c24d4
Clover <irfanifaiddd@gmail.com>
2024-01-24 09:25:45 +0700

    splash, login,main app

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Splash from './src/Splash'; // Import the Splash component
+import MainAppScreen from './src/MainAppScreen'; // Import your main app screen component
+import LoginScreen from './src/LoginScreen';
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" headerMode="none">
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="MainApp" component={MainAppScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
